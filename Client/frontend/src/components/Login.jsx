5632e2e99056f6b0eaa526f470314520406bee69
Satwik Biswas <148995066+satwikcoder007@users.noreply.github.com>
2024-02-29 15:14:54 +0000

    Update Login.jsx

--- Client/frontend/src/components/Login.jsx
+++ Client/frontend/src/components/Login.jsx
@@ -4,7 +4,12 @@ export default function Login(props) {
   return (
     <div className=" h-screen w-screen flex justify-center items-center">
       <div className=" relative  box-border h-[400px] w-[500px] bg bg-blue-400 text-white">
-        <button className="absolute top-3 right-3 box-border px-[15px] py-[5px] text-white rounded-md bg-black" onClick={()=>{props.closeSign()}}>
+        <button
+          className="absolute top-3 right-3 box-border px-[15px] py-[5px] text-white rounded-md bg-black"
+          onClick={() => {
+            props.closeSign();
+          }}
+        >
           close
         </button>
       </div>
